ab8afd79d1608f20fc8e2181b5edc00b5b1d5d68
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-02-23 21:24:47 +0700

    end

--- pertemuan1.cpp
+++ pertemuan1.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 //variabel global
 int sks;
+
+int main(){
+    //variabel global
+    int umur;
+    umur = 20;
+
+    cout << "Hallo Mas Asep" << endl;
+    cout << 10 << endl;
+    cout << umur;
+}
